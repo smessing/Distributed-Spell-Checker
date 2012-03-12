fe9566a78edee81d253d1b9f1e85d1b283161249
Samuel Messing <samuel.messing@gmail.com>
2012-03-12 01:46:29 -0400

    adding in stub file, plus todo's.

---
+++ src/SpellCheckerTester.java
@@ -0,0 +1,46 @@
+import java.util.Iterator;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+
+/*
+ * File created on: Mar 12, 2012
+ * Author: Samuel Messing <sbm2158@columbia.edu>
+ */
+
+public class SpellCheckerTester extends TestCase {
+	
+	public SpellCheckerTester(String name) {
+		super(name);
+	}
+	
+
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	
+	/*
+	 * Ideas for tomorrow:
+	 * - Flesh out this jUnit test as a means to generate statistics on performance.
+	 * - Generate statistics on speed (probably handle in individual tests).
+	 * - See about accuracy/speed tradeoff to calculating edit distance 3.
+	 * - Read Norvig's notes.
+	 * - Develop distributed design.
+	 * 
+	 * 
+	 * Bored?
+	 * - Mine Guava's collections for useful things.
+	 *
+	 */
+	
+	
+	public void evaluateSpellChecker1() {
+		
+	}
+	
+
+}
