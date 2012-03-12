9f8ba5fbaa1d04b8e8e6866dbf6418f1ad6bc41e
Samuel Messing <samuel.messing@gmail.com>
2012-03-12 15:55:40 -0400

    starting refactoring of code to separate out SpellChecker and ConsoleSpellChecker.

---
+++ src/spellchecker/ConsoleSpellChecker.java
@@ -0,0 +1,18 @@
+package spellchecker;
+
+/*
+ * File created on: Mar 12, 2012
+ * Author: Samuel Messing <sbm2158@columbia.edu>
+ */
+
+public class ConsoleSpellChecker extends SpellChecker {
+
+	protected static SpellChecker parent;
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
